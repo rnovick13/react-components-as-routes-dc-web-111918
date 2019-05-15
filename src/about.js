bdd9f62f0cb3be9db732d2714bd936e45f78544a
rnovick13 <rnovick13@gmail.com>
2019-05-15 13:52:42 -0400

    Done.

---
+++ src/about.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+class About extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>This is my about component!</h1>
+      </div>
+    );
+  }
+}
+
+export default About
